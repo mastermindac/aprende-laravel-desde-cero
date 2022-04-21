9da3ea4a9348a9737766dbe46bcd73f1075dfb0e
Antonio Sarosi <sarosiantonio@gmail.com>
2022-04-21 21:05:15 +0200

    Add unique email for contact validation

--- app/Http/Requests/StoreContactRequest.php
+++ app/Http/Requests/StoreContactRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreContactRequest extends FormRequest
 {
@@ -25,10 +26,23 @@ class StoreContactRequest extends FormRequest
     {
         return [
             'name' => 'required',
-            'email' => 'required|email',
+            'email' => [
+                'required', 
+                'email',
+                Rule::unique('contacts', 'email')
+                    ->where('user_id', auth()->id())
+                    ->ignore(request()->route('contact'))
+            ],
             'phone_number' => 'required|digits:9',
             'age' => 'required|numeric|min:1|max:255',
             'profile_picture' => 'image|nullable',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.unique' => 'You already have a contact with this email'
         ];
     }
 }
